248c5bf68cc0b243c360fb6596278f01749b9125
Ntheus <matheusoliveira.7313@gmail.com>
2023-04-19 13:09:47 -0300

    app main

--- IMC/src/App.java
+++ IMC/src/App.java
@@ -1,5 +1,10 @@
+import view.ImcView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        ImcView imcView = new ImcView();
+        imcView.imcUi();
+
     }
 }
