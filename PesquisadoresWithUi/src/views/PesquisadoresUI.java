d7c28ebb0219bb12b52d7cbeb48d00d52f493bb7
Ntheus <matheusoliveira.7313@gmail.com>
2023-05-03 02:07:42 -0300

    abas e labels

--- PesquisadoresWithUi/src/views/PesquisadoresUI.java
+++ PesquisadoresWithUi/src/views/PesquisadoresUI.java
@@ -1,6 +1,8 @@
 package views;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -20,9 +22,26 @@ public class PesquisadoresUI {
         }
 
         myFrame = new JFrame("Pesquisadores");
-        myFrame.setBounds(0, 0, 450, 600);
+        myFrame.setBounds(0, 0, 450, 500);
+
+        JLabel nomedoNovoPesquisadorLabel = new JLabel("Pesquisador:");
+        nomedoNovoPesquisadorLabel.setBounds(20, 30, 75, 25);
+
+        JLabel tipodoNovoPesquisaodorLabel = new JLabel("Tipo:");
+        tipodoNovoPesquisaodorLabel.setBounds(20, 70, 50, 25);
+
+        JLabel textodaSelecaodeCoordenadorLabel = new JLabel("Coordenador:");
+        textodaSelecaodeCoordenadorLabel.setBounds(20, 110, 90, 25);
+
+        JButton botaoCadastrar = new JButton("Cadastrar");
+
         JTabbedPane tabbedPane = new JTabbedPane();
+
         JPanel cadastrodePesquisadoresPanel = new JPanel(null);
+        cadastrodePesquisadoresPanel.add(nomedoNovoPesquisadorLabel);
+        cadastrodePesquisadoresPanel.add(tipodoNovoPesquisaodorLabel);
+        cadastrodePesquisadoresPanel.add(textodaSelecaodeCoordenadorLabel);
+
         JPanel consultadePesquisadoresPanel = new JPanel(null);
         tabbedPane.add("Cadastro", cadastrodePesquisadoresPanel);
         tabbedPane.add("Consulta", consultadePesquisadoresPanel);
