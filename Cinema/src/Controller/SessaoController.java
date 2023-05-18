2c91f90b7dca70e2f4a7dcaac7bb730fe40e69fc
Ntheus <matheusoliveira.7313@gmail.com>
2023-05-18 13:37:35 -0300

    controle da venda

--- Cinema/src/Controller/SessaoController.java
+++ Cinema/src/Controller/SessaoController.java
@@ -1,9 +1,17 @@
 package Controller;
 
+import model.Sessoes;
+
 public class SessaoController {
 
-    SessaoController() {
-
+    public static Sessoes controledeVendaDeIngressos(Sessoes sessao, int quantidadedeIngressosaSeremComprados)
+            throws Exception {
+        if (sessao.getTotaldeIngressosVendidos() + quantidadedeIngressosaSeremComprados > sessao
+                .getQuantidadeDeIngressosTotalDisponiveisaVenda()) {
+            throw new Exception("Quantidade excede a quantidade de ingressos disponiveis");
+        }
+        sessao.vendeIngresso(quantidadedeIngressosaSeremComprados);
+        return sessao;
     }
 
 }
