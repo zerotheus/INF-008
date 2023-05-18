f458686d8e49b66cc4fd884e59c6d220d72dd5aa
Ntheus <matheusoliveira.7313@gmail.com>
2023-05-18 15:20:35 -0300

    comboBox das salas

--- Cinema/src/view/CinemaUI.java
+++ Cinema/src/view/CinemaUI.java
@@ -1,6 +1,11 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import model.Cinema;
 
@@ -12,12 +17,19 @@ public class CinemaUI {
     public void UIdoCinema() {
 
         cinema = new Cinema();
+        JLabel SelecioneAsala = new JLabel("Sala Numero:");
+        SelecioneAsala.setBounds(20, 30, 35, 25);
+        Integer[] NumerodasSalas = cinema.getSalasIds();
+        JComboBox<Integer> comboBoxdeSelecaodeSala = new JComboBox<Integer>(NumerodasSalas);
+        comboBoxdeSelecaodeSala.setBounds(65, 30, 35, 25);
 
         frame = new JFrame("Cinema");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(500, 450);
         frame.setVisible(true);
+        frame.add(SelecioneAsala);
+        frame.add(comboBoxdeSelecaodeSala);
     }
 
 }
