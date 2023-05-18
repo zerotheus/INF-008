7cba00d1ac04a83305cb123d0c505079c04beae6
Ntheus <matheusoliveira.7313@gmail.com>
2023-05-18 13:44:01 -0300

    Main

---
+++ Cinema/src/view/App.java
@@ -0,0 +1,12 @@
+package view;
+
+import model.Cinema;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        CinemaUI cinemaUI = new CinemaUI();
+        cinemaUI.UIdoCinema();
+    }
+}
