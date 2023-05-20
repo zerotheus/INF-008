6dc3201ddcb36a55701546c3a8a664876a1949ad
Ntheus <matheusoliveira.7313@gmail.com>
2023-05-20 17:22:21 -0300

    validacao de valor

--- Cinema/src/Controller/SessaoController.java
+++ Cinema/src/Controller/SessaoController.java
@@ -6,6 +6,9 @@ public class SessaoController {
 
     public static Sessoes controledeVendaDeIngressos(Sessoes sessao, int quantidadedeIngressosaSeremComprados)
             throws Exception {
+        if (quantidadedeIngressosaSeremComprados < 0) {
+            throw new Exception("Nao se pode vender uma quantidade negativa");
+        }
         if (sessao.getTotaldeIngressosVendidos() + quantidadedeIngressosaSeremComprados > sessao
                 .getQuantidadeDeIngressosTotalDisponiveisaVenda()) {
             throw new Exception("Quantidade excede a quantidade de ingressos disponiveis");
